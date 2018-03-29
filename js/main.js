0cdfbcb1141f797dff46e4486425c76c315e4850
Anton Lantukh <lantukh@bk.ru>
2018-03-29 12:22:19 +0400

    Убрал IIFE + правки по функции рендера экрана

--- js/main.js
+++ js/main.js
@@ -1,66 +1,59 @@
-(function () {
+// Константы с ключами клавиш
+const keyCode = {
+  ARROW_LEFT: 37,
+  ARROW_RIGHT: 39
+};
 
-  // Константы с ключами клавиш
-  const keyCode = {
-    ARROW_LEFT: 37,
-    ARROW_RIGHT: 39
-  };
+// Константы с кол-вом страниц
+const screensArray = {
+  MAX_NUMBER: 6,
+  MIN_NUMBER: 0
+};
 
-  // Константы с кол-вом страниц
-  const screensArray = {
-    MAX_NUMBER: 6,
-    MIN_NUMBER: 0
-  };
+// Переменные под смену экранов
+const screenMain = document.querySelector(`.central`);
+const currentScreen = document.createDocumentFragment();
+const templatesPack = document.querySelectorAll(`template`);
+let screenFullPack = [];
 
-  // Сохраняем стартовый экран
-  const screenMain = document.querySelector(`.central`);
-  const currentScreen = document.createDocumentFragment();
+// Сораняем стартовый экран
+Array.from(screenMain.cloneNode(true).children).forEach((item) => currentScreen.appendChild(item));
 
-  Array.from(screenMain.cloneNode(true).children).forEach((item) => currentScreen.appendChild(item));
+// Собираем все содержимое шаблонов в массив
+const screenTemplates = Array.from(templatesPack).map((item) => item.content.cloneNode(true));
+screenFullPack.push(currentScreen.cloneNode(true));
+screenFullPack = screenFullPack.concat(screenTemplates);
 
-  // Функция смены экрана
-  const changeScreens = (screenNumber) => {
-    let screenFullPack = [];
-    // Собираем все содержимое шаблонов в массив
-    const screenTemplates = Array.from(document.querySelectorAll(`template`)).map((item) => {
-      return item.content.cloneNode(true);
-    });
+// Функция смены экрана
+const changeScreens = (screenNumber) => {
+  screenMain.innerHTML = ``;
+  screenMain.appendChild(screenFullPack[screenNumber].cloneNode(true));
+};
 
-    // Объединяем в общий массив
-    screenFullPack.push(currentScreen.cloneNode(true));
-    screenFullPack = screenFullPack.concat(screenTemplates);
+// Функция обработки переключения экранов
+let screenNumber = 0;
+const onArrowPress = () => {
+  if (!event.altKey) {
+    return;
+  }
 
-    // Чистим содежримое тега main
-    screenMain.innerHTML = ``;
-    screenMain.appendChild(screenFullPack[screenNumber]);
-  };
-
-  // Функция обработки переключения экранов
-  let screenNumber = 0;
-  const onArrowPress = () => {
-
-    if (!event.altKey) {
-      return;
+  if (event.keyCode === keyCode.ARROW_LEFT) {
+    if (screenNumber <= screensArray.MIN_NUMBER) {
+      screenNumber = screensArray.MIN_NUMBER;
+    } else {
+      screenNumber--;
     }
+    changeScreens(screenNumber);
 
-    if (event.keyCode === keyCode.ARROW_LEFT) {
-      if (screenNumber <= screensArray.MIN_NUMBER) {
-        screenNumber = screensArray.MIN_NUMBER;
-      } else {
-        screenNumber--;
-      }
-      changeScreens(screenNumber);
-
-    } else if (event.keyCode === keyCode.ARROW_RIGHT) {
-      if (screenNumber >= screensArray.MAX_NUMBER) {
-        screenNumber = screensArray.MAX_NUMBER;
-      } else {
-        screenNumber++;
-      }
-      changeScreens(screenNumber);
+  } else if (event.keyCode === keyCode.ARROW_RIGHT) {
+    if (screenNumber >= screensArray.MAX_NUMBER) {
+      screenNumber = screensArray.MAX_NUMBER;
+    } else {
+      screenNumber++;
     }
-  };
+    changeScreens(screenNumber);
+  }
+};
 
-  // Обработчик переключений экранов при нажатии на клавиши
-  document.addEventListener(`keydown`, onArrowPress);
-})();
+// Обработчик переключений экранов при нажатии на клавиши
+document.addEventListener(`keydown`, onArrowPress);
